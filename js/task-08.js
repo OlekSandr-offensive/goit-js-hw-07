9bb53efcef0117def9d35d850fa48a4775a9944d
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-04-28 12:35:44 +0200

    доробив 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,5 @@
 const createDiv = document.querySelectorAll('#controls button');
-//const clearDiv = document.querySelectorAll('#controls button');
+
 const mainDivBox = document.querySelector('#boxes');
 const dataInput = document.querySelector('#controls input');
 
@@ -13,17 +13,24 @@ createDiv[0].addEventListener('click', () => {
 createDiv[1].addEventListener('click', () => {
   console.log('clearDiv clear');
   dataInput.value = null;
+  mainDivBox.innerHTML = '';
 });
+
+const getRandomRgbNum = () => Math.floor(Math.random() * 256);
+
 const createBoxes = amount => {
   const countArray = Array(Number(amount)).fill(0);
-  // console.log(amount + typeof Number(amount));
+
   const restart = countArray.map((elem, index) => {
     const createMyDiv = document.createElement('div');
     createMyDiv.style.width = `${30 + index * 10}px`;
     createMyDiv.style.height = `${30 + index * 10}px`;
-    createMyDiv.style.background = 'red';
+    const red = getRandomRgbNum();
+    const green = getRandomRgbNum();
+    const blue = getRandomRgbNum();
+    createMyDiv.style.backgroundColor = `rgb(${red},${green},${blue})`;
     createMyDiv.style.border = 'black 2px solid';
-    // mainDivBox.appendChild(createMyDiv);
+
     return createMyDiv;
   });
   mainDivBox.append(...restart);
